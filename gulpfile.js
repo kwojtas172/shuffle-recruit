41e5c0d3f3e58706a42f4514e119f0e9651a6b11
kwojtas172 <k.wojtas172@gmail.com>
2021-08-23 14:12:20 +0200

    added autoprefixer for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
+// const del = require('del');
 
 gulp.task('styles', () => {
     return gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/'));
+        
 });
 
+gulp.task('autoprefixer', () => {
+    return gulp.src('./css/index.css')
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./css/'));
+})
 // gulp.task('clean', () => {
 //     return del([
 //         'css/main.css',
@@ -17,7 +24,8 @@ gulp.task('styles', () => {
 
 gulp.task('watch', () => {
     gulp.watch('sass/**/*.scss', (done) => {
-        gulp.series(['styles'])(done);
+        gulp.series(['styles', 'autoprefixer'])(done);
+        
     });
 });
 
